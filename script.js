5a0b23e20e85b9a3725b93c4488acf69609e5c66
Nicolas Umuc <nicolas.webvision@gmail.com>
2024-09-05 17:29:48 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,69 @@
+// MENU DE NAVIGATION 
+const menuToggle = document.getElementById('menu-toggle');
+const navList = document.querySelector('.nav-list');
+menuToggle.addEventListener('click', () => {
+    document.body.classList.toggle('menu-active');
+});
+
+// ETOILES FILANTE ET ASTEROIDE 
+
+function createShootingStar() {
+    console.log('createShootingStar called'); // Vérifie que la fonction est appelée
+
+    const starImages = [
+        'images/etoile.png',
+        'images/asteroide.png'
+    ];
+
+    const shootingStar = document.createElement('div');
+    shootingStar.className = 'shooting-star';
+
+    const randomImage = starImages[Math.floor(Math.random() * starImages.length)];
+    shootingStar.style.backgroundImage = `url('${randomImage}')`;
+
+    const size = Math.random() * 30 + 20;
+    shootingStar.style.width = `${size}px`;
+    shootingStar.style.height = `${size * 0.6}px`;
+
+    const starX = Math.random() * window.innerWidth;
+    const starY = Math.random() * window.innerHeight;
+
+    shootingStar.style.left = `${starX}px`;
+    shootingStar.style.top = `${starY}px`;
+
+    document.getElementById('shooting-stars').appendChild(shootingStar);
+
+    setTimeout(() => {
+        shootingStar.remove();
+    }, 2000);
+}
+
+setInterval(createShootingStar, Math.random() * 3000 + 1000);
+
+// POP UP PAGE PLANETE CARACTERISTIQUES DES PLANETES
+
+document.addEventListener('DOMContentLoaded', function() {
+    const buttons = document.querySelectorAll('.btn-characteristics');
+    const popups = document.querySelectorAll('.popup');
+    const closeButtons = document.querySelectorAll('.popup .close');
+
+    buttons.forEach(button => {
+        button.addEventListener('click', () => {
+            const target = button.getAttribute('data-target');
+            document.getElementById(target).style.display = 'flex';
+        });
+    });
+
+    closeButtons.forEach(close => {
+        close.addEventListener('click', () => {
+            close.closest('.popup').style.display = 'none';
+        });
+    });
+
+    // Fermer la popup en cliquant en dehors de son contenu
+    window.addEventListener('click', (event) => {
+        if (event.target.classList.contains('popup')) {
+            event.target.style.display = 'none';
+        }
+    });
+});
